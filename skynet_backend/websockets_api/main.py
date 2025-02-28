4279215d9ac6379230a5cea712bc7532e32ff6fa
crucials <ensueness@gmail.com>
2025-02-28 12:40:09 +0500

    replace raw websockets with socket.io

--- skynet_backend/websockets_api/main.py
+++ skynet_backend/websockets_api/main.py
@@ -1,26 +1,11 @@
-import asyncio
-
-from websockets import ServerConnection
-from websockets.asyncio.server import serve
+import uvicorn
+import socketio
 
 from skynet_backend.logging_config import root_logger
 
 
-async def start_llm_conversation(websocket_server_connection: ServerConnection):
-    if (
-        websocket_server_connection.request is not None
-        and websocket_server_connection.request.path != "/api/llm-conversation"
-    ):
-        return
+socketio_server = socketio.AsyncServer()
 
-    await websocket_server_connection.send("Test message")
-
-
-async def start_websockets_server():
-    root_logger.info("Starting websockets server")
-
-    async with serve(start_llm_conversation, host="localhost", port=8000) as server:
-        await server.serve_forever()
-
-
-asyncio.run(start_websockets_server())
+if __name__ == "__main__":
+    root_logger.info("Starting Socket.io server via uvicorn")
+    uvicorn.run(socketio.ASGIApp(socketio_server))
