a35ac05185ce663c6e0958facea80774e3892801
crucials <ensueness@gmail.com>
2025-03-02 17:28:43 +0500

    socket.io server: enable cors

---
+++ skynet_backend/websockets_api/config.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+ENV_FILE_PATH = os.environ.get("SKYNET__ENV_FILE") or "./.env"
+
+logger = logging.getLogger(__name__)
+
+
+class WebsocketsApiConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=ENV_FILE_PATH,
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+        env_prefix="SKYNET__",
+        extra="ignore",
+    )
+
+    cors_allowed_origins: list[str]
+
+    @staticmethod
+    def load_from_env():
+        return WebsocketsApiConfig.model_validate({})
+
+
+logger.info("Loading config (.env) from '%s'", ENV_FILE_PATH)
+websockets_api_config = WebsocketsApiConfig.load_from_env()
--- skynet_backend/websockets_api/socketio_server.py
+++ skynet_backend/websockets_api/socketio_server.py
@@ -1,4 +1,8 @@
 import socketio
 
+from skynet_backend.websockets_api.config import websockets_api_config
 
-socketio_server = socketio.AsyncServer(async_mode="asgi")
+
+socketio_server = socketio.AsyncServer(
+    async_mode="asgi", cors_allowed_origins=websockets_api_config.cors_allowed_origins
+)
