0bebd21e00e0829d4ef3e10f8d55e2122cd90d3e
crucials <ensueness@gmail.com>
2025-03-06 15:17:11 +0500

    add ws api poetry script, wrap ws api main file in a function

--- skynet_backend/websockets_api/main.py
+++ skynet_backend/websockets_api/main.py
@@ -6,8 +6,8 @@ from skynet_backend.websockets_api.events import register_socketio_events
 from skynet_backend.websockets_api.socketio_server import socketio_server
 
 
-register_socketio_events(socketio_server)
+def main():
+    register_socketio_events(socketio_server)
 
-if __name__ == "__main__":
     root_logger.info("Starting Socket.io server via uvicorn")
     uvicorn.run(socketio.ASGIApp(socketio_server))
