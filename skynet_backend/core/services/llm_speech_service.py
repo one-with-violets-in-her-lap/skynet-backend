02f3938df1cb02be38d61679c2e497dc8cc935c9
one-with-violets-in-her-lap <walkways@proton.me>
2025-04-22 21:10:04 +0500

    fix "rate limited" gpt messages output by stopping to reuse g4f client instance

--- skynet_backend/core/services/llm_speech_service.py
+++ skynet_backend/core/services/llm_speech_service.py
@@ -1,8 +1,11 @@
 import logging
-import g4f
 import timeit
 
-from skynet_backend.common.api_clients.responsive_voice.client import ResponsiveVoiceClient
+import g4f
+
+from skynet_backend.common.api_clients.responsive_voice.client import (
+    ResponsiveVoiceClient,
+)
 from skynet_backend.common.errors import ExternalApiError
 from skynet_backend.core.models.llm_conversation import ConversationParticipantModelName
 from skynet_backend.core.models.llm_message import LlmMessage, LlmMessageWithSpeech
@@ -14,10 +17,8 @@ logger = logging.getLogger(__name__)
 class LlmSpeechService:
     def __init__(
         self,
-        g4f_client: g4f.AsyncClient,
         responsive_voice_client: ResponsiveVoiceClient,
     ):
-        self.g4f_client = g4f_client
         self.responsive_voice_client = responsive_voice_client
 
     async def get_llm_speech_reply(
@@ -25,7 +26,9 @@ class LlmSpeechService:
         message_history: list[LlmMessage],
         talking_model_name: ConversationParticipantModelName,
     ):
-        response = await self.g4f_client.chat.completions.create(
+        g4f_client = g4f.AsyncClient()
+
+        response = await g4f_client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[message.model_dump() for message in message_history],
             web_search=False,
@@ -37,7 +40,8 @@ class LlmSpeechService:
             )
 
         logger.info(
-            "LLM responses: %s", [llm_response.message for llm_response in response.choices]
+            "LLM responses: %s",
+            [llm_response.message for llm_response in response.choices],
         )
         message = response.choices[0].message
 
