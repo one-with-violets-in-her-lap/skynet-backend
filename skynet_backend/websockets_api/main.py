a85639dcf3300a2a1ac5c76a8af7ae6f21e9fd2c
one-with-violets-in-her-lap <walkways@proton.me>
2025-04-30 16:43:57 +0500

    implement rate limiting

--- skynet_backend/websockets_api/main.py
+++ skynet_backend/websockets_api/main.py
@@ -1,14 +1,15 @@
 import logging
 
+from ratelimit import Rule, RateLimitMiddleware
+from ratelimit.auths.ip import client_ip
+from ratelimit.backends.simple import MemoryBackend
 import uvicorn
 import socketio
 
 from skynet_backend.websockets_api.events import register_socketio_events
+from skynet_backend.websockets_api.config import websockets_api_config
 from skynet_backend.websockets_api.socketio_server import socketio_server
 from skynet_backend.websockets_api.config import websockets_api_config
-
-
-root_logger = logging.getLogger()
 
 
 def main():
@@ -18,14 +19,29 @@ def main():
         datefmt="%m/%d/%Y %I:%M:%S %p",
     )
 
+    root_logger = logging.getLogger()
+
     register_socketio_events(socketio_server)
 
+    socketio_asgi_app = socketio.ASGIApp(
+        socketio_server,
+        static_files={"/public": "./public"},
+    )
+
+    if websockets_api_config.enable_rate_limiting:
+        socketio_asgi_app = RateLimitMiddleware(
+            socketio_asgi_app,
+            client_ip,
+            MemoryBackend(),
+            {
+                r"^/*": [Rule(day=1, group="default")],
+            },
+        )
+
     root_logger.info("Starting Socket.io server via uvicorn")
+    
     uvicorn.run(
-        socketio.ASGIApp(
-            socketio_server,
-            static_files={"/public": "./public"},
-        ),
+        socketio_asgi_app,
         host=websockets_api_config.host,
         port=websockets_api_config.port,
     )
