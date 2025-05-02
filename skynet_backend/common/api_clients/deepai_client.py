774be6ed09c6121a1d9ff12256a24f38911aa445
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-05-02 07:08:28 +0500

    deepai client: clean up request headers

--- skynet_backend/common/api_clients/deepai_client.py
+++ skynet_backend/common/api_clients/deepai_client.py
@@ -33,13 +33,6 @@ class DeepaiClient:
                 "origin": "https://deepai.org",
                 "pragma": "no-cache",
                 "priority": "u=1, i",
-                "sec-ch-ua": '"Brave";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": '"Linux"',
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-site",
-                "sec-gpc": "1",
                 "user-agent": FAKE_USER_AGENT,
             },
             data={
